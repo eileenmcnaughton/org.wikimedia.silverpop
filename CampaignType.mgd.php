67472dc10472a70a913b686aeca11e8c0c9a6037
eileen <emcnaughton@wikimedia.org>
2017-05-01 13:19:12 +1200

    Add Campaign type managed entity

--- CampaignType.mgd.php
+++ CampaignType.mgd.php
@@ -5,3 +5,16 @@
  * Date: 4/27/17
  * Time: 1:32 PM
  */
+return array(
+  'campaign_type' => array(
+    'name' => 'campaign_type',
+    'entity' => 'OptionValue',
+    'cleanup' => 'never',
+    'params' => array(
+      'version' => 3,
+      'label' => 'Email',
+      'name' => 'Email',
+      'option_group_id' => 'campaign_type',
+    ),
+  ),
+);
